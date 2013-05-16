c616eaf31ceed6a64cd0ede80a72cbc68c18c970
Brian Myungjune JUNG <blusjune+github@gmail.com>
2013-05-16 14:43:10 +0900

    re-architecturing;

--- db/wikini/mysql_wikidb_update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-ln -s /x/t/share tmp;
-_tstamp=$(ls -1 .tstamp.* | sed -e 's/\.tstamp\.\(.*\)/\1/g');
-_sql_wikidb="wikidb-${_tstamp}.sql";
-cat sql_dump/_sql_* > tmp/$_sql_wikidb;
-echo "drop database wikidb; create database wikidb;" | mysql -p -u root;
-mysql -p -u root wikidb < tmp/$_sql_wikidb;
