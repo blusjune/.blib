8b73e21c15ae75040f15299a5a10aaddbd375d64
Brian Myungjune JUNG <blusjune+github@gmail.com>
2013-05-24 19:01:33 +0900

    wikini;wikilogo;

--- wiki/wikini/mysql_wikidb_update.sh
+++ wiki/wikini/mysql_wikidb_update.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
-ln -s /x/t/share tmp;
-_tstamp=$(ls -1 .tstamp.* | sed -e 's/\.tstamp\.\(.*\)/\1/g');
-_sql_wikidb="wikidb-${_tstamp}.sql";
+#_ver=20130524_185559;
+
+if [ ! -d tmp ]; then
+	if [ -d /x/t/share ]; then
+		ln -s /x/t/share tmp;
+	else
+		mkdir tmp;
+	fi
+fi
+_tstamp_file=$(ls -1 .tstamp.* | sed -e 's/\.tstamp\.\(.*\)/\1/g');
+_sql_wikidb="wikidb-${_tstamp_file}.sql";
+echo "#>> merging DB ...";
 cat sql_dump/_sql_* > tmp/$_sql_wikidb;
+echo "#>> drop and re-create wikidb ...";
 echo "drop database wikidb; create database wikidb;" | mysql -p -u root;
+echo "#>> import wikidb from $_sql_wikidb ...";
 mysql -p -u root wikidb < tmp/$_sql_wikidb;
